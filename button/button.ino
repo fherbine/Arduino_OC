0e0017c13ab4d1581eafac4d158108debfee2c0c
fherbine <felix.herbinet@yahoo.com>
2018-05-01 19:52:24 +0200

    Updating button

--- button/button.ino
+++ button/button.ino
@@ -1,11 +1,17 @@
 void  setup(void)
 {
   pinMode(7, INPUT);
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
+  digitalWrite(13, LOW);
 }
 
 void  loop(void)
 {
   boolean state = digitalRead(7);
+  if (state == 1)
+    digitalWrite(13, HIGH);
+  else
+    digitalWrite(13, LOW);
   Serial.println(state);
 }
