3d8d1f3603b9ca6c998523095c45a22f6d6ecf74
fherbine <felix.herbinet@yahoo.com>
2018-05-01 17:39:31 +0200

    Update blink13

--- blink13/blink13.ino
+++ blink13/blink13.ino
@@ -1,12 +1,15 @@
 void  setup(void)
 {
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
 
 void  loop(void)
 {
   digitalWrite(13, HIGH);
+  Serial.println("State HIGH");
   delay(500);
   digitalWrite(13, LOW);
+  Serial.println("State LOW");
   delay(500);
 }
