02b016c220d971fc44816ee76ea83725d9406e4e
fherbine <felix.herbinet@yahoo.com>
2018-05-12 03:04:52 +0200

    Updating pot

--- pot/pot.ino
+++ pot/pot.ino
@@ -1,5 +1,5 @@
 const int led = 13;
-int       a0_val = 0, bSpeed = 0;
+int       a0_val = 0, bSpeed = 0, test = 0;
 
 void setup(void)
 {
@@ -10,8 +10,12 @@ void setup(void)
 void loop(void)
 {
   a0_val = analogRead(A0);
-  Serial.println(a0_val);
-  blinky_13(a0_val);
+  // blinky_13(a0_val); // first led blinking test
+  test = map(a0_val, 0, 1023, 1, 5);
+  Serial.print(a0_val);
+  Serial.print(" -> ");
+  Serial.println(test);
+  blinky2_13(test);
 }
 
 void blinky_13(int aValue)
@@ -21,8 +25,29 @@ void blinky_13(int aValue)
     else
       bSpeed = (aValue < 100) ? 100 : aValue;
 
-    digitalWrite(led, HIGH);
-    delay(bSpeed);
-    digitalWrite(led, LOW);
-    delay(bSpeed);
+    do_blink(bSpeed);
+}
+
+void blinky2_13(int mapped_val)
+{
+  if (mapped_val == 1)
+    bSpeed = 100;
+  else if (mapped_val == 2)
+    bSpeed = 200;
+  else if (mapped_val == 3)
+    bSpeed = 300;
+  else if (mapped_val == 4)
+    bSpeed = 400;
+  else if (mapped_val == 5)
+    bSpeed = 500;
+    
+  do_blink(bSpeed);
+}
+
+void  do_blink(int l13Speed)
+{
+  digitalWrite(led, HIGH);
+  delay(l13Speed);
+  digitalWrite(led, LOW);
+  delay(l13Speed);
 }
