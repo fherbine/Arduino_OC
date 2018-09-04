2803f31562ce54d1846e41460c0666d747a59da3
fherbine <felix.herbinet@yahoo.com>
2018-09-04 17:19:15 +0200

    Updating servo project

--- Servo/Servo.ino
+++ Servo/Servo.ino
@@ -1,15 +1,43 @@
+#include <Servo.h>
+
+/*  class     */
+Servo M1;
 /*  globales  */
-int g_testVar = 0;
+int  g_potVal = 0;
+short  g_pwmServo = 3;
+int g_period = 20000;
+
+
+void servoMotorSetAngle(int rotation)
+{
+  rotation = map(rotation, 0, 180, 500, 2500);
+
+  digitalWrite(g_pwmServo, LOW);
+
+  for (int t = 0; t < 300; t++)
+  {
+    digitalWrite(g_pwmServo, HIGH);
+    delayMicroseconds(rotation);
+    digitalWrite(g_pwmServo, LOW);
+    delayMicroseconds(g_period - rotation);
+  }
+}
 
 void setup()
 {
+  pinMode(g_pwmServo, OUTPUT);
+  digitalWrite(g_pwmServo, LOW);
+  M1.attach(8);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  g_testVar = analogRead(A0);
-  g_testVar = map(g_testVar, 0, 1023, 0, 180);
+  g_potVal = analogRead(A0);
+  g_potVal = map(g_potVal, 0, 1023, 0, 180);
   Serial.print("Val: ");
-  Serial.println(g_testVar);
+  Serial.println(g_potVal);
+  //servoMotorSetAngle(g_potVal);
+  M1.write(g_potVal);
+  delay(1000);
 }
